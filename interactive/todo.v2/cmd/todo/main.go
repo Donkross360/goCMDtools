7a96b2bd907c098b78d131cb9fcd1b64e3e9fc9c
Mart Young <mart2great@yahoo.com>
2025-01-06 08:49:48 +0100

    Todo v2: Improved Usage infomation

---
+++ interactive/todo.v2/cmd/todo/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"interactive/todo.v2"
+)
+
+// Hardcoding the file name
+const todoFileName = ".todo.json"
+
+func main() {
+
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "%s tool. Developed by Donkross\n", os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "Copyright 2025\n")
+		fmt.Fprintln(flag.CommandLine.Output(), "Usage Information:")
+		flag.PrintDefaults()
+	}
+
+	// Parsing command line flags
+	task := flag.String("task", "", "task to be included in the Todo list")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+
+	flag.Parse()
+
+	// Define an items list
+	l := &todo.List{}
+
+	// use the Get command to read to do items from file
+	if err := l.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	// Define what to do based on the provided flag
+	switch {
+	case *list:
+		// list current to do items
+		for _, item := range *l {
+			if !item.Done {
+				fmt.Println(item.Task)
+			}
+		}
+
+	case *complete > 0:
+		// Complete the given item
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+	case *task != "":
+		// Add the task
+		l.Add(*task)
+
+		// save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+	default:
+		// Invalid flag provided
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
+
+	}
+}
---
+++ interactive/todo.v2/go.mod
@@ -0,0 +1,3 @@
+module interactive/todo.v2
+
+go 1.22.2
